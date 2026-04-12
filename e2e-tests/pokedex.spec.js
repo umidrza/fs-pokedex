2c464a62cc7ccef400e7646cf78ac1b7c54ff81f
umidrza <umidrza4@gmail.com>
2026-04-13 00:10:03 +0400

    start app before e2e tests

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -2,7 +2,7 @@ import { test, describe, expect } from "@playwright/test";
 
 describe("Pokedex", () => {
   test("front page can be opened", async ({ page }) => {
-    await page.goto("");
+    await page.goto("/");
     await expect(page.getByText("ivysaur")).toBeVisible();
     await expect(
       page.getByText(
