dc0280c77849e26a6e237a6024ca599145a36add
umidrza <umidrza4@gmail.com>
2026-04-13 13:21:01 +0400

    simulate a broken deployment

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+
   res.send('ok')
 })
 
